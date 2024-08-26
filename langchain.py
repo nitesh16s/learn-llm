7190010ebdc4b7ab99ba8d492025821bad5bc362
niteshh16s <nitesh.saini@pratechbrands.com>
2024-08-26 13:53:41 +0530

    init

---
+++ langchain.py
@@ -0,0 +1,69 @@
+import json
+from langchain_community.document_loaders import UnstructuredPDFLoader
+from langchain_community.document_loaders import OnlinePDFLoader
+
+local_path = './lekl101.pdf'
+
+loader = UnstructuredPDFLoader(file_path=local_path)
+data = loader.load()
+
+# print(data[0].page_content)
+
+# Vector Embeddings
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+
+# Split and chunk 
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+chunks = text_splitter.split_documents(data)
+
+# Add to vector database
+vector_db = Chroma.from_documents(
+    documents=chunks, 
+    embedding=OllamaEmbeddings(model="nomic-embed-text",show_progress=True),
+    collection_name="local-rag"
+)
+
+from langchain.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.chat_models import ChatOllama
+from langchain_core.runnables import RunnablePassthrough
+from langchain.retrievers.multi_query import MultiQueryRetriever
+
+local_model = "mistral"
+llm = ChatOllama(model=local_model)
+
+QUERY_PROMPT = PromptTemplate(
+    input_variables=["question"],
+    template="""You are an AI language model assistant. Your task is to generate five
+    different versions of the given user question to retrieve relevant documents from
+    a vector database. By generating multiple perspectives on the user question, your
+    goal is to help the user overcome some of the limitations of the distance-based
+    similarity search. Provide these alternative questions separated by newlines.
+    Original question: {question}""",
+)
+retriever = MultiQueryRetriever.from_llm(
+    vector_db.as_retriever(), 
+    llm,
+    prompt=QUERY_PROMPT
+)
+
+# RAG prompt
+template = """Answer the question based ONLY on the following context:
+{context}
+Question: {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+while True:
+    a = chain.invoke(input("query: "))
+    print('a: ', a)
+# chain.invoke("What are the 5 pillars of global cooperation?")
---
+++ llama_index.py
@@ -0,0 +1,21 @@
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.ollama import Ollama
+
+documents = SimpleDirectoryReader("data").load_data()
+print(documents)
+
+# bge-base embedding model
+Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+
+# ollama
+Settings.llm = Ollama(model="llama3", request_timeout=360.0)
+
+index = VectorStoreIndex.from_documents(
+    documents,
+)
+
+query_engine = index.as_query_engine()
+while True:
+    response = query_engine.query(input("query: "))
+    print(response)
